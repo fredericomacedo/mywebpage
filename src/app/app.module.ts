3af0326cffd8fada61228bde33947355bcd2fcef
Frederico Lúcio Macedo <fredvalentim.macedo@gmail.com>
2023-09-29 15:07:01 -0400

    main page with separete footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
