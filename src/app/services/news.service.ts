901ec560339fecc44b10a273b9a16460bf69cdd7
Frederico Lúcio Macedo <fredvalentim.macedo@gmail.com>
2023-11-08 14:33:58 -0500

    build with news api resource

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class NewsService {
 
-  private baseUrl = 'http://localhost:3000';  // Change to your server's address and port if different
-  //private baseUrl = 'https://alphaserverapi-bcbe02aa8105.herokuapp.com';
+  //private baseUrl = 'http://localhost:3000';  // Change to your server's address and port if different
+  private baseUrl = 'https://market-news-api-b456dd0c70b8.herokuapp.com/news/tech';
 
   constructor(private http: HttpClient) { }
 
   getNews(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/news`);
+    return this.http.get<any>(`${this.baseUrl}`);
   }
 }
