d6fa8aa1e7a61bbbd266c943ab528518b8f2838b
Frederico Lúcio Macedo <fredvalentim.macedo@gmail.com>
2023-11-16 14:22:20 -0500

    add dynamic copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TawkToService } from './services/TawkToService';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-webpage';
+  constructor(private tawktoService: TawkToService){}
+  ngOnInit(){
+      this.tawktoService.initializeTawkTo();
+  }
 }
---
+++ src/app/services/TawkToService.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TawkToService {
+
+  constructor() { }
+
+  initializeTawkTo() {
+    const s1 = document.createElement('script');
+    s1.async = true;
+    s1.src = `https://embed.tawk.to/${environment.tawkToKey}`;
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+
+    const s0 = document.getElementsByTagName('script')[0];
+
+    if (s0 && s0.parentNode) {
+      s0.parentNode.insertBefore(s1, s0);
+    } else {
+      document.head.appendChild(s1);
+    }
+  }
+}
+
