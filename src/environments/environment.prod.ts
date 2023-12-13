26389ed40e83070fd23386f799ce3a3ce8d241ae
Frederico Lúcio Macedo <fredvalentim.macedo@gmail.com>
2023-12-13 13:08:47 -0500

    add environments directory

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    // ... other environment variables
+    production: true,
+    tawkToKey: '65551d41958be55aeaaff481/1hfa7kinj'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    // ... other environment variables
+    production: false,
+    tawkToKey: '65551d41958be55aeaaff481/1hfa7kinj'
+  };
+  
